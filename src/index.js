42f2a394cd6dfe4bd1acef9256621768a81af677
mvmagnussen <magnus.magnussen@ciber.no>
2018-05-30 20:15:45 +0200

    added scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AddButton from "./components/AddButton";
+import "./styles/styles.scss";
 
 const app = document.getElementById("app");
 
