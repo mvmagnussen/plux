ec34d92a80874b8df9c7616e191cae47276b0310
mvmagnussen <magnus.magnussen@ciber.no>
2018-06-03 15:35:02 +0200

    first version to deploy

--- src/components/PluxApp/PluxApp.js
+++ src/components/PluxApp/PluxApp.js
@@ -1,10 +1,78 @@
 import React from "react";
 
 class PluxApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailVisible: false
+    };
+    this.newRandomGreeting = this.newRandomGreeting.bind(this);
+  }
+
+  newRandomGreeting() {
+    this.setState({
+      emailVisible: true
+    });
+    const greeting = document.getElementById("greeting");
+    greeting.innerHTML = this.randomGreeting();
+  }
+
+  randomGreeting() {
+    const pAdjectives = [
+      "pålitelig",
+      "passende",
+      "pyntelig",
+      "perfekt",
+      "presis",
+      "pragmatisk",
+      "prangende",
+      "produktiv",
+      "prippen",
+      "pusete",
+      "praktisk",
+      "pitoresk",
+      "personlig",
+      "pikant",
+      "poetisk",
+      "prikkfri",
+      "presentabel"
+    ];
+    const lAdjectives = [
+      "liten",
+      "lønnsom",
+      "lydig",
+      "lovende",
+      "lykkelig",
+      "livlig",
+      "lojal",
+      "lattermild",
+      "lekker",
+      "lystig",
+      "leken",
+      "legitim",
+      "lokkende",
+      "luftig",
+      "lydig",
+      "lovende",
+      "logisk"
+    ];
+    const randomP = pAdjectives[Math.floor(Math.random() * pAdjectives.length)];
+    const randomL = lAdjectives[Math.floor(Math.random() * lAdjectives.length)];
+    return `${randomP} ${randomL} ux`;
+  }
+
   render() {
     return (
-      <div className="plux-app">
-        <button onClick={() => alert("Plux dude")}>PLUX</button>
+      <div id="plux-app">
+        <div id="greeting" role="button" onClick={this.newRandomGreeting}>
+          {this.randomGreeting()}
+        </div>
+        <a
+          href="mailto:hei@plux.no"
+          className={`email ${this.state.emailVisible && "visible"}`}
+        >
+          hei@plux.no
+        </a>
       </div>
     );
   }
